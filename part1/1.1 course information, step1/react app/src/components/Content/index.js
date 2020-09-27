4b330f4af5db80fa016dd1f0b641a77751b915d7
mfialko <mykhailof@tallium.com>
2020-09-27 20:24:53 +0300

    added part1 exercises - 1.2 course information, step2

--- part1/1.1 course information, step1/react app/src/components/Content/index.js
+++ part1/1.1 course information, step1/react app/src/components/Content/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
+//components
+import Part from './Part';
 
 
 const Content = ({content}) => {
 
     return (
-        <>
+        <div>
             {content.map((item) => {
-                return (<p key={'part' + item.partNum}>
-                    {item.partName} {item.exercisesNum}
-                </p>)
+                return (
+                    <Part 
+                        key={'part' + item.partNum}
+                        name={item.partName}
+                        num={item.exercisesNum} 
+                    />
+                )
             })}
-        </>
+        </div>
     )
 }
 
