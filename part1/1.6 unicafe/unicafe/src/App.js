80f8c707fffc215ddc41a15e7929ec238c60d3ad
mfialko <mykhailof@tallium.com>
2020-11-28 17:35:42 +0300

    exercise 1.8 unicafe

--- part1/1.6 unicafe/unicafe/src/App.js
+++ part1/1.6 unicafe/unicafe/src/App.js
@@ -4,23 +4,7 @@ const Button = ({ label, handleClick }) => {
   return <button onClick={handleClick}>{label}</button>
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const addGoodFeedback = () => {
-    setGood(prev => prev + 1);
-  }
-
-  const addNeutralFeedback = () => {
-    setNeutral(prev => prev + 1);
-  }
-
-  const addBadFeedback = () => {
-    setBad(prev => prev + 1);
-  }
+const Statistics = ({good, neutral, bad}) => {
 
   const getAverageScore = () => {
     let scores = {
@@ -40,6 +24,40 @@ const App = () => {
   }
 
   return (
+    <div className="statistics">
+      <h2>statistics</h2>
+      <ul className="feedback-list">
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        <li>all {good + neutral + bad}</li>
+        <li>average {getAverageScore()}</li>
+        <li>positive {getPositivePercent()}</li>
+      </ul>
+    </div>
+  );
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const addGoodFeedback = () => {
+    setGood(prev => prev + 1);
+  }
+
+  const addNeutralFeedback = () => {
+    setNeutral(prev => prev + 1);
+  }
+
+  const addBadFeedback = () => {
+    setBad(prev => prev + 1);
+  }
+
+
+  return (
     <div>
       <h1>give feedback</h1>
       <div className="buttons">
@@ -47,17 +65,7 @@ const App = () => {
         <Button label="neutral" handleClick={addNeutralFeedback} />
         <Button label="bad" handleClick={addBadFeedback} />
       </div>
-      <div className="statistics">
-        <h2>statistics</h2>
-        <ul className="feedback-list">
-          <li>good {good}</li>
-          <li>neutral {neutral}</li>
-          <li>bad {bad}</li>
-          <li>all {good + neutral + bad}</li>
-          <li>average {getAverageScore()}</li>
-          <li>positive {getPositivePercent()}</li>
-        </ul>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
