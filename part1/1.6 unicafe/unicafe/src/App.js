8252735af818572558f3623781cc9eec9e5f851b
mfialko <mykhailof@tallium.com>
2020-11-28 17:42:06 +0300

    exercise 1.9 unicafe

--- part1/1.6 unicafe/unicafe/src/App.js
+++ part1/1.6 unicafe/unicafe/src/App.js
@@ -1,42 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Button = ({ label, handleClick }) => {
-  return <button onClick={handleClick}>{label}</button>
-}
+  return <button onClick={handleClick}>{label}</button>;
+};
 
-const Statistics = ({good, neutral, bad}) => {
-
+const Statistics = ({ good, neutral, bad }) => {
   const getAverageScore = () => {
     let scores = {
       good: 1,
       neutral: 0,
-      bad: -1
-    }
-    let sumOfScores = good * scores.good + neutral * scores.neutral + bad * scores.bad;
+      bad: -1,
+    };
+    let sumOfScores =
+      good * scores.good + neutral * scores.neutral + bad * scores.bad;
     let numOfScores = good + neutral + bad;
-    return numOfScores > 0 ? (sumOfScores)/numOfScores : 0;
-  }
+    return numOfScores > 0 ? sumOfScores / numOfScores : 0;
+  };
 
   const getPositivePercent = () => {
     let numOfScores = good + neutral + bad;
-    let positive = numOfScores > 0 ? (good/numOfScores) : 0;
+    let positive = numOfScores > 0 ? good / numOfScores : 0;
     return `${positive * 100}%`;
-  }
+  };
 
   return (
     <div className="statistics">
       <h2>statistics</h2>
-      <ul className="feedback-list">
-        <li>good {good}</li>
-        <li>neutral {neutral}</li>
-        <li>bad {bad}</li>
-        <li>all {good + neutral + bad}</li>
-        <li>average {getAverageScore()}</li>
-        <li>positive {getPositivePercent()}</li>
-      </ul>
+      {good + neutral + bad === 0 ? (
+        <span>No feedback given</span>
+      ) : (
+        <ul className="feedback-list">
+          <li>good {good}</li>
+          <li>neutral {neutral}</li>
+          <li>bad {bad}</li>
+          <li>all {good + neutral + bad}</li>
+          <li>average {getAverageScore()}</li>
+          <li>positive {getPositivePercent()}</li>
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -45,17 +49,16 @@ const App = () => {
   const [bad, setBad] = useState(0);
 
   const addGoodFeedback = () => {
-    setGood(prev => prev + 1);
-  }
+    setGood((prev) => prev + 1);
+  };
 
   const addNeutralFeedback = () => {
-    setNeutral(prev => prev + 1);
-  }
+    setNeutral((prev) => prev + 1);
+  };
 
   const addBadFeedback = () => {
-    setBad(prev => prev + 1);
-  }
-
+    setBad((prev) => prev + 1);
+  };
 
   return (
     <div>
@@ -67,7 +70,7 @@ const App = () => {
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
